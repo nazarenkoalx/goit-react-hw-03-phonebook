b65b0ca13cf9a869a4d00840c2515ecd88c4b55b
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-03-03 11:30:00 +0200

    base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Component } from 'react';
+import { Filter } from './Filter/Filter';
+import { Container } from './Container/Container.styled';
+import { nanoid } from 'nanoid';
+
+const INITIAL_STATE = {
+  contacts: [],
+  filter: '',
 };
+
+export class App extends Component {
+  state = { ...INITIAL_STATE };
+
+  addContact = newContact => {
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      return alert(
+        `nope, ${newContact.name} is already added to contact list =(`
+      );
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+      };
+    });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  searchContact = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const filterInLowerCase = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterInLowerCase)
+    );
+  };
+
+  render() {
+    const fileredContacts = this.filterContacts();
+    return (
+      <Container>
+        <ContactForm onSubmit={this.addContact} />
+        <Filter onChange={this.searchContact} contacts={fileredContacts} />
+        <ContactList contacts={fileredContacts} onDelete={this.deleteContact} />
+      </Container>
+    );
+  }
+}
